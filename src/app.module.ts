2c32488360384a103e3989b9f54da4dd62cb9e4d
hl.lee@quintet.co.kr <hgoguma124@gmail.com>
2022-08-21 12:47:01 +0900

    board service, controller 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { BoardsController } from './boards/boards.controller';
 import { BoardsModule } from './boards/boards.module';
 
 @Module({
   imports: [BoardsModule],
-  controllers: [BoardsController],
 })
 export class AppModule {}
--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,10 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
   //service dependency injection
-
   // before
   // boardService: boardService; // 먼저 프로퍼티 선언
   // constructor(private boardService: boardService) {
@@ -12,7 +11,12 @@ export class BoardsController {
   // }
 
   // after (한줄로 요약)
-  // 접근 제한자(public, private, protected)를 생성자(constructor) 파라미터에 선언하면
+  // **접근 제한자(public, private, protected)를 생성자(constructor) 파라미터에 선언하면
   // 접근 제한자가 사용된 생성자 파라미터는 암묵적으로 클래스의 프로퍼티로 선언됨!
   constructor(private boardService: BoardsService) {}
+
+  @Get('/')
+  getAllBoard() {
+    return this.boardService.getAllBoards();
+  }
 }
