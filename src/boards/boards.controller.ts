0b46bd38c8a0504eea9acbf0f3f2a70b8945cf7d
OPENGA_Dev_hllee-at-900616242684 <hl.lee@quintet.co.kr>
2022-09-05 17:41:09 +0900

    custom decorator 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -9,18 +9,22 @@ import {
   UsePipes,
   ValidationPipe,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { BoardStatus } from './board-status.enum';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 import { Board } from './board.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('boards')
+@UseGuards(AuthGuard())
 export class BoardsController {
   constructor(private boardService: BoardsService) {}
 
-  @Get('/') // 실제 경로는 '/boards'로 셋팅됨
+  // 실제 경로는 '/boards'로 셋팅됨
+  @Get('/')
   getAllBoard(): Promise<Board[]> {
     return this.boardService.getAllBoards();
   }
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -3,9 +3,10 @@ import { BoardRepository } from './board.repository';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 import { TypeOrmExModule } from '../database/typeorm-ex.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmExModule.forCustomRepository([BoardRepository])],
+  imports: [TypeOrmExModule.forCustomRepository([BoardRepository]), AuthModule],
   // imports: [TypeOrmModule.forFeature([BoardRepository])],
   controllers: [BoardsController],
   providers: [BoardsService],
